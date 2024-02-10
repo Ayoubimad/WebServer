8f12ab6934889aeb25199ef7dc3f3d6e6b386a4f
Ayoub Imad <285322@studenti.unimore.it>
2024-02-10 21:02:05 +0100

    comments and 404 Not found page added

--- REST/models.py
+++ REST/models.py
@@ -5,18 +5,38 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
+    """
+    Model to store contact information.
+    """
     phoneNumber = models.CharField(default=None, max_length=20)
 
 
 class User(AbstractUser):
+    """
+    Custom user model extending AbstractUser.
+
+    Attributes:
+        profile_pic (ImageField): Profile picture of the user.
+        vehicles (ManyToManyField): Vehicles associated with the user.
+        contacts (ManyToManyField): Contacts associated with the user for alerts.
+    """
     profile_pic = models.ImageField(upload_to='profile_pic/')
     vehicles = models.ManyToManyField('Vehicle', related_name='owners')
-    contacts = models.ManyToManyField('Contact', related_name='alert_contatcs')
+    contacts = models.ManyToManyField('Contact', related_name='alert_contacts')
 
 
 class Vehicle(models.Model):
+    """
+    Model to store vehicle information.
+
+    Attributes:
+        id (IntegerField): Primary key for the vehicle.
+        latitude (FloatField): Latitude coordinate of the vehicle.
+        longitude (FloatField): Longitude coordinate of the vehicle.
+        smoke (FloatField): Smoke level detected by the vehicle.
+        temperature (FloatField): Temperature recorded by the vehicle.
+    """
     id = models.IntegerField(primary_key=True)
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vehicles')
     latitude = models.FloatField(default=0)
     longitude = models.FloatField(default=0)
     smoke = models.FloatField(default=0)
@@ -24,6 +44,20 @@ class Vehicle(models.Model):
 
 
 class Alert(models.Model):
+    """
+    Model to store alert information.
+
+    Attributes:
+        sender (ForeignKey): Vehicle sending the alert.
+        receivers (ManyToManyField): Vehicles receiving the alert.
+        latitude (FloatField): Latitude coordinate of the alert.
+        longitude (FloatField): Longitude coordinate of the alert.
+        smoke (FloatField): Smoke level detected by the alert.
+        temperature (FloatField): Temperature recorded by the alert.
+        date (DateTimeField): Date and time when the alert was created.
+        recent (BooleanField): Flag indicating if the alert is recent or not.
+    """
+
     sender = models.ForeignKey(Vehicle, related_name='alerts_sent', on_delete=models.CASCADE)
     receivers = models.ManyToManyField(Vehicle, related_name='alerts_received', default=None)
     latitude = models.FloatField(default=0)
@@ -37,8 +71,11 @@ class Alert(models.Model):
         """
         Returns all vehicles within a certain radius from this alert.
 
-        :param radius: The radius within which to search for vehicles (in kilometers).
-        :return: A queryset containing the vehicles within the specified radius.
+        Args:
+            radius (float): The radius within which to search for vehicles (in kilometers).
+
+        Returns:
+            list: A list containing the vehicles within the specified radius.
         """
         alert_point = (self.latitude, self.longitude)
 
--- REST/urls.py
+++ REST/urls.py
@@ -4,7 +4,11 @@ from REST.views import *
 
 urlpatterns = [
     path('vehicles/', VehiclesAPI.as_view(), name='vehicle-api'),
+    # Endpoint to get all vehicles or create a new one
     path('vehicles/<int:vehicle_id>/', VehiclesAPI.as_view(), name='vehicle-api-id'),
+    # Endpoint to get, update or delete a specific vehicle by its ID
     path('alerts/', AlertsAPI.as_view(), name='alert-api'),
-    path('contatcs/<int:vehicle_id>', UserAPI.as_view()),
+    # Endpoint to get all alerts or create a new one
+    path('contacts/<int:vehicle_id>', UserAPI.as_view()),
+    # Endpoint to get contacts related to a specific vehicle
 ]
